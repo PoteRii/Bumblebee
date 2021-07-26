24cc329a26d14942562e68e6e93f9beb6ef98ab8
Giorgi Martiashvili <Giorgi.Martiashvili@space.ge>
2021-07-26 18:34:14 +0400

    add fullsearch js

---
+++ Bumblebee/fullSearch.js
@@ -0,0 +1,33 @@
+﻿module.exports = function (callback, content, keyWord, searchInProcessParams) {
+
+    var process = function (content, keyWord, searchInProcessParams) {
+        var data = content;
+        var containingProcesses = [];
+
+        if (searchInProcessParams) {
+            for (var i = 0; i < data.length; i++) {
+                var item = data[i];
+
+                if (item.conv_type == "process") {
+                    if (item.params) {
+                        if (item.params.filter(param => param.name.toLowerCase().includes(keyWord.toLowerCase())).length > 0) {
+                            containingProcesses.push(
+                                {
+                                    processId: item.obj_id,
+                                    processName: item.title
+                                });
+                        }
+                    }
+                }
+            }
+        }
+
+
+      
+        
+
+        return JSON.stringify(containingProcesses);
+    }
+
+    callback(null, process(JSON.parse(content), keyWord, searchInProcessParams));
+}
